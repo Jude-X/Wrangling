a67f6c68c3998bab6edadcf3b621c387e6670bd8
Jude-X <nzubechukwu@flutterwavego.com>
2021-07-31 15:36:25 +0100

    added logs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #import pandas module for data wrangling
+from datetime import datetime
 import pandas as pd
 
 def get_csv(url):
@@ -8,10 +9,14 @@ def get_csv(url):
     Country, Year, Area, Population, GDP per capita, Population density, 
     Vehicle ownership, Total road deaths, and Road deaths per Million Inhabitants.
     '''
+    #Keep track of time
+    start = datetime.now()
     ## Read Html from url
+    print("\n Reading Url")
     df = pd.read_html(url)
     
     ## Select the table with an index of its position
+    print("\n Performing wrangling")
     df = df[2]
     
     #Edit column names to match requirements
@@ -35,14 +40,18 @@ def get_csv(url):
     #remove non numeric characters
     for col in df.columns[2:]:
         if df[col].dtype == "O":
-            df[col] = df[col].str.replace(r'\D+', '')
+            df[col] = df[col].str.replace(r'\D+', '', regex=True)
   
     ##Sort by Road deaths per Million Inhabitants column
     df = df.sort_values("Road deaths per Million Inhabitants").reset_index(drop=True)
-    
+
+    print("\n Copying csv to disk")
     df.to_csv("data.csv",index=False)
     
+    #print time taken
+    print("\n Time Taken: ", (datetime.now() - start).total_seconds(), " second(s)")
     return df
 
 url = 'https://en.wikipedia.org/wiki/Road_safety_in_Europe'
-get_csv(url).head(20)
+
+get_csv(url)
